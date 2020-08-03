9fa11b3fab46d00ac8dd7784b3d9441421c57911
marcelovinhas <marcelovinhascosta@hotmail.com>
2020-08-03 11:27:44 -0300

    1 - Configurações iniciais

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import './config/ReactotronConfig';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>EASY</Text>
+    </View>
+  );
+}
